45479d8cfaad90a0e7c7f864be0cba97828d3eb6
Wanderson Moura Oliveira <wmouraol@everis.com>
2021-01-21 22:15:06 -0300

    adicionado url defalt para chamadas e criando formGroup para login

--- src/app/feature/login/login.component.ts
+++ src/app/feature/login/login.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'page-login',
+  selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  control: FormGroup;
+
+  constructor(private fb : FormBuilder) {
+    this.control = fb.group({});
+   }
 
   ngOnInit(): void {
+    this.control = this.fb.group({
+      login: ['', [Validators.required]],
+      currentPassword: ['', [Validators.required]]
+    });
+  }
+
+  onSubmit(){
+    console.log("chegou");
   }
 
 }
+
