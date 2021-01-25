876e4f8c044b16878598218d4d1277eee4d060ca
Pedro Machado Tannus <pmachadt@everis.com>
2021-01-25 11:33:05 -0300

    Ajustes no roteamento

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +9,9 @@ import { Component } from '@angular/core';
 export class AppComponent {
   labelBtn = 'Entrar';
   title = 'dwfrontoffice';
+
+  constructor(public router: Router){
+
+  }
+
 }
