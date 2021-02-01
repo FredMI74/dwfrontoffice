d53c179e509756ff9735f90e4a9d55f4aa2e48fe
Frederico <fmonteni@everis.com>
2021-02-01 10:35:03 -0300

    Consulta desejos

--- src/app/feature/wishes/list-wishes/list-wishes.component.ts
+++ src/app/feature/wishes/list-wishes/list-wishes.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { AddOfferComponent } from '@feature/offers/add-offer/add-offer.component';
+import { FormControl, FormGroup} from '@angular/forms';
+import { WhishesService } from '../wishes.service';
 
 @Component({
   selector: 'page-list-wishes',
@@ -8,15 +10,40 @@ import { AddOfferComponent } from '@feature/offers/add-offer/add-offer.component
   styleUrls: ['./list-wishes.component.css']
 })
 export class ListWishesComponent implements OnInit {
+  
+  listForm: FormGroup;
 
-  constructor(private modalService: NgbModal) { }
+  headElements = ['Cód', 'Icone', 'Descrição', 'Cidade','Tipo Prod.', 'Ofertas Vál.','Ação'];
 
-  openModal(id_wish: number) {
+  elements: any;
+
+
+  constructor(private modalService: NgbModal, private wishesService :  WhishesService) {
+
+    this.listForm = new FormGroup({
+      descricao: new FormControl('')
+    });
+   }
+
+  openModal(id_wish: string) {
     const modalRef = this.modalService.open(AddOfferComponent);
    modalRef.componentInstance.id_wish = id_wish;
   }
 
-  ngOnInit(): void {
+  ngOnInit(): void { }
+
+  onSubmit() {
+    console.log('data = ', this.listForm.controls.descricao?.value );
+    
+    this.wishesService
+      .list(
+        this.listForm.get('descricao')?.value)
+      .subscribe((response) => {
+        if (response.resultado.erro === false) {
+            console.log(response);
+            this.elements = response.conteudo;
+        }
+      });
   }
 
 }
